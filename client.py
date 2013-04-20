edafe3fcd1511d7191a51c793e31c6a8be425a20
Ajith <ajkedlaya@gmail.com>
2013-04-19 20:31:52 -0500

    fresh changes

---
+++ client.py
@@ -0,0 +1,81 @@
+from flask import render_template, Flask, request
+import urllib2, json, pprint
+import url_builder
+
+app = Flask(__name__)
+
+#Variables - globals
+json_response = ""
+
+#URL Form : https://api.remix.bestbuy.com/v1/products(search=phones)?apiKey=bkrmw5nuvtje73guc2v8mt8q&format=json
+baseUrl = "https://api.remix.bestbuy.com/v1/products"
+
+@app.route('/')
+def index():
+    return render_template('request.html')
+
+
+@app.route('/results/', methods=['POST'])
+def show_results():
+    url_builder.data['searchString'] = ""
+    if request.method == 'POST':
+        
+        categoryButton = request.form['category']
+
+        if categoryButton == 'phone':
+            print "Phone category button clicked \n"
+            url_builder.data['searchString'] += 'phone%20'
+            
+            choiceButton = request.form['display']
+             
+            if choiceButton == 'latest':
+                print "Phone latest choiceButton clicked \n"
+                #url_builder.data['searchString'] += 'retina%20display%20'
+
+            elif choiceButton == 'newish':
+                print "Phone newishButton clicked \n"
+                #url_builder.data['searchString'] += 'AMOLED%20display%20'
+            
+            elif choiceButton == 'old':
+                print "Phone old choiceButton clicked \n"
+                #url_builder.data['searchString'] += 'normal%20display%20'
+            pass
+        pass
+    pass
+    
+    return next_page()
+
+@app.route('/next/')
+def next_page():
+    myUrl = baseUrl
+    myUrl = url_builder.add_search_string(myUrl)
+    myUrl = url_builder.add_empty_query_string(myUrl)
+    myUrl = url_builder.add_api_key_to_query_string(myUrl)
+    myUrl = url_builder.add_format_to_query_string(myUrl)
+    myUrl = url_builder.add_next_page_number(myUrl)
+    print "MY URL" + myUrl + "\n" 
+    response = urllib2.urlopen(myUrl)
+    json_response = json.load(response)
+    return render_template('results.html', json_response = json_response )
+
+@app.route('/previous/')
+def previous_page():
+    myUrl = baseUrl
+    myUrl = url_builder.add_search_string(myUrl)
+    myUrl = url_builder.add_empty_query_string(myUrl)
+    myUrl = url_builder.add_api_key_to_query_string(myUrl)
+    myUrl = url_builder.add_format_to_query_string(myUrl)
+    myUrl = url_builder.add_previous_page_number(myUrl)
+    print "MY URL" + myUrl + "\n" 
+    response = urllib2.urlopen(myUrl)
+    json_response = json.load(response)
+    return render_template('results.html', json_response = json_response)
+
+if __name__ == '__main__':
+    url_builder.data['searchString'] = url_builder.searchString
+    url_builder.data['bestBuyKey'] = url_builder.bestBuyKey
+    url_builder.data['formatString'] = url_builder.formatString
+    url_builder.data['page'] = 0
+    app.run(debug=True)
+    pass
+
---
+++ url_builder.py
@@ -0,0 +1,35 @@
+import urllib2, json, pprint
+
+#Variables - globals
+
+searchString = ""
+bestBuyKey = "bkrmw5nuvtje73guc2v8mt8q"
+formatString = "json"
+data = {}
+
+
+def add_empty_query_string(baseUrl):
+    return baseUrl + "?"
+
+def add_search_string(baseUrl) :
+    return baseUrl + "(search=" + data['searchString'] + ")"
+
+def add_api_key_to_query_string(baseUrl):
+    return baseUrl + "&apiKey=" + data['bestBuyKey']
+
+def add_format_to_query_string(baseUrl):
+    return baseUrl + "&format=" + data['formatString']
+
+def add_next_page_number(baseUrl):
+    if data['page'] >= 0 :
+        data['page'] = data['page'] + 1
+        baseUrl = baseUrl + "&page=" + str(data['page'])
+    
+    return baseUrl
+
+def add_previous_page_number(baseUrl):
+    if data['page'] > 1 :
+        data['page'] = data['page'] - 1;
+        baseUrl = baseUrl + "&page=" + str(data['page'])
+
+    return baseUrl
